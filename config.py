ef0be5a2954c3b3f57f3f7408c8c7ccf7f938254
Dylan Wright <dylmwrig@iu.edu>
2019-12-05 21:51:03 -0500

    Some changes to get it running on windows again

--- config.py
+++ config.py
@@ -9,9 +9,14 @@ screen = pygame.display.set_mode((CANVAS_WIDTH, CANVAS_HEIGHT))
 
 
 # loading in sprites should probably happen here??
-punch_img = pygame.image.load("/home/fuwafuwatime/PycharmProjects/Game-Dev-Final/assets/punch.png")
-chop_img = pygame.image.load("/home/fuwafuwatime/PycharmProjects/Game-Dev-Final/assets/chop.png")
-headbutt_img = pygame.image.load("/home/fuwafuwatime/PycharmProjects/Game-Dev-Final/assets/headbutt.png")
+punch_img = pygame.image.load("D:\School\Game Dev with Andy\Final\\assets\punch.png")
+chop_img = pygame.image.load("D:\School\Game Dev with Andy\Final\\assets\chop.png")
+headbutt_img = pygame.image.load("D:\School\Game Dev with Andy\Final\\assets\headbutt.png")
+
+#linux
+#punch_img = pygame.image.load("/home/fuwafuwatime/PycharmProjects/Game-Dev-Final/assets/punch.png")
+#chop_img = pygame.image.load("/home/fuwafuwatime/PycharmProjects/Game-Dev-Final/assets/chop.png")
+#headbutt_img = pygame.image.load("/home/fuwafuwatime/PycharmProjects/Game-Dev-Final/assets/headbutt.png")
 
 # easy enemy formations, use these in the beginning
 simpleEnemyFormations = [["", "", "", "Samurai"]]
